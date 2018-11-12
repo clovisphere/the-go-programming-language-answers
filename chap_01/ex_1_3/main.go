029c11b7835524c4dd58375ccae22ee3fab3d1a7
Clovis Mugaruka <clovis.mugaruka@gmail.com>
2018-11-12 14:27:25 +0300

    fixed logging

--- chap_01/ex_1_3/main.go
+++ chap_01/ex_1_3/main.go
@@ -13,26 +13,25 @@ import (
 func timeTrack(start time.Time, name string) {
 	elpased := time.Since(start)
 	log.Printf("%s took: %s", name, elpased)
-	fmt.Println()
-
+    fmt.Println()
 }
 
 // starts program execution
 func main() {
 	start := time.Now()
-	for i := 0; i < len(os.Args); i++ {
+	for i := 1; i < len(os.Args); i++ {
 		fmt.Println(os.Args[i])
 	}
-	timeTrack(start, "'loop'")
+	timeTrack(start, "'[loop]'")
 
 	start = time.Now()
-	for _, arg := range os.Args {
+    for _, arg := range os.Args[1:] {
 		fmt.Println(arg)
 	}
-	timeTrack(start, "'for+range'")
+	timeTrack(start, "'[for+range]'")
 
 	start = time.Now()
-	fmt.Println(strings.Join(os.Args, "\n"))
-	timeTrack(start, "'join'")
+    fmt.Println(strings.Join(os.Args[1:], "\n"))
+	timeTrack(start, "'[join]'")
 
 }
