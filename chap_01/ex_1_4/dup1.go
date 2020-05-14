2a56e4ed14827dd0990ea39a438ae3584e7ddcaf
clov <clovis.mugaruka@gmail.com>
2020-05-14 12:35:08 +0300

    added code for dup example - 'find duplicate lines'

---
+++ chap_01/ex_1_4/dup1.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	counts := make(map[string]int)
+	input := bufio.NewScanner(os.Stdin)
+
+	fmt.Println("Type <q> to end program")
+
+	for input.Scan() {
+		// gracefully break out of loop
+		if input.Text() == "q" {
+			break
+		}
+		counts[input.Text()]++
+	}
+	// NOTE: ignoring potential errors from input.Err()
+	for line, n := range counts {
+		if n > 1 {
+			fmt.Printf("%d\t%s\n", n, line)
+		}
+	}
+}
